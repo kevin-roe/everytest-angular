15d2db799bf1607bee80a2860b4e8f4ab6d5ffb5
Kevin Roe <kroe761@gmail.com>
2020-12-15 15:37:37 -0600

    added server

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  private REST_API_SERVER = "http://localhost:3001/";
+  private REST_API_SERVER = environment.api_url;
 
   constructor(private httpClient: HttpClient) { }
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  api_url: "https://still-coast-96203.herokuapp.com/"
 };
