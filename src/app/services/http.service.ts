c331f4ae451d5a01a8c6b4a9ad788300eacc0347
Kevin Roe <kroe761@gmail.com>
2020-12-16 07:47:37 -0600

    logging

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -11,7 +11,8 @@ export class HttpService {
 
   constructor(private httpClient: HttpClient) { }
 
-  public get<T>(path: string){
+  public get<T>(path: string) {
+    console.log(this.REST_API_SERVER + path)
     return this.httpClient.get<T>(this.REST_API_SERVER + path, { withCredentials: true });
   }
 
