79d5583e2fa0327d8ce45951701affac33d8b2a8
Kevin Roe <kroe761@gmail.com>
2020-12-21 09:00:57 -0600

    added spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
 import { AuthService } from './services/auth.service';
 
 @Component({
@@ -8,8 +8,19 @@ import { AuthService } from './services/auth.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'everytest-angular';
+  showLoadingSpinner = false;
   public_routes = ["/", "/login", "/register"]
 
-  constructor(public authService: AuthService, public router: Router) { }
+  constructor(public authService: AuthService, public router: Router) {
+    
+    this.router.events.subscribe((e: Event) => {
+      if (e instanceof NavigationStart) {
+        this.showLoadingSpinner = true;
+      }
+
+      if (e instanceof NavigationEnd || e instanceof NavigationCancel || e instanceof NavigationError) {
+        this.showLoadingSpinner = false;
+      }
+    })
+  }
 }
